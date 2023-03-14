783908540ac5ff7fbe6d69f355cfbdff12b880d1
hafizakbar19 <hafizakbar19@gmail.com>
2023-03-14 08:55:29 +0500

    q4 edited

--- task4.ts
+++ task4.ts
@@ -3,7 +3,8 @@ function factorial(num: number) {
     let result = 1;
     for (let i = 2; i <= num; i++) {
       result *= i;
+      console.log(result, "*");
     }
     return result;
   }
-  console.log(factorial(5));
+  factorial(3);
